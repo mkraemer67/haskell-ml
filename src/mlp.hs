1b2ee2ba7a65614fd53c8ab08f4fed1d53d645f0
Martin Krämer <mkraemer.de@gmail.com>
2015-03-19 06:56:17 +0100

    initial skeleton

--- src/mlp.hs
+++ src/mlp.hs
@@ -1,9 +1,11 @@
 import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.LAPACK
+import Numeric.LinearAlgebra.Data
 
 data Layer = Layer {
     nNeurons   :: Int,
-    activation :: Double -> Double
+    activation :: Double -> Double,
+    weights    :: Vector Double
 }
 
 linearLayer :: Int -> Layer
@@ -11,9 +13,18 @@ linearLayer n = Layer { nNeurons = n, activation = id }
 
 type Network = [Layer]
 
+_activate :: Vector Double -> Layer -> Vector Double
+_activate x layer = x  -------- TODO
+
+activate :: Vector Double -> Network -> Vector Double
+activate = foldl _activate
+
 main :: IO()
 main = do
-    let
-        a = (1000><1000) $ replicate (1000*1000) 1.0
-        b = (1000><1000) $ replicate (1000*1000) 1.0
-    print $ (a `multiplyR` b) @@> (900,900)
+    let a = linearLayer 10
+    let b = linearLayer 20
+    let c = linearLayer 2
+    let net = [a,b,c]
+    let x = vector [1..10]
+    let y = activate x net
+    print y
