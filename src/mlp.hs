3490896924004f431eb73c8a6ab81ed9b3632f9c
Martin Krämer <mkraemer.de@gmail.com>
2015-03-19 15:11:43 +0100

    refactoring

--- src/mlp.hs
+++ src/mlp.hs
@@ -2,8 +2,13 @@
 
 import Data.List
 import Debug.Trace
-import Numeric.LinearAlgebra
+import Numeric.LinearAlgebra hiding (Matrix, Vector)
+import qualified Numeric.LinearAlgebra as LA
 import System.Random.Mersenne
+
+type FpType = Double
+type Vector = LA.Vector FpType
+type Matrix = LA.Matrix FpType
 
 -- Generators
 
@@ -17,26 +22,27 @@ sigmoidLayer :: Int -> Layer
 sigmoidLayer n = Layer { neurons = n, activation = sigm, layerType = "sigmoid" }
     where sigm x = 1 / (1 + exp (-x))
 
-mlp :: [Layer] -> [Matrix Double] -> Network
+-- TODO: It would be nicer to enforce #layers == #weights+1 by zipping.
+mlp :: [Layer] -> [WeightMatrix] -> Network
 mlp ls ws = debug Network { layers = ls, weights = ws, netType = "mlp" }
-    where debug = trace $ "Creating MLP\n" ++ intercalate "\n" text ++ "\n"
-          text  = map (\x -> "Layer " ++ show (fst x) ++ ": " ++ show (snd x)) (zip [1..] ls)
+    where text  = map (\x -> "Layer " ++ show (fst x) ++ ": " ++ show (snd x)) (zip [1..] ls)
+          debug = trace $ "Creating MLP\n" ++ intercalate "\n" text ++ "\n"
 
 -- Initializer
 
-initVector :: MTGen -> Int -> IO (Vector Double)
+initVector :: MTGen -> Int -> IO (Vector)
 initVector g n = do
     rs <- randoms g
     let v = fromList (take n rs)
     return v
 
-initMatrix :: MTGen -> Int -> Int -> IO (Matrix Double)
+initMatrix :: MTGen -> Int -> Int -> IO (Matrix)
 initMatrix g n m = do
-    rs <- randoms g :: IO [Double]
+    rs <- randoms g :: IO [FpType]
     let v = (n><m) $ take (n*m) rs
     return v
 
-initConnections :: MTGen -> (Layer, Layer) -> IO (Matrix Double)
+initConnections :: MTGen -> (Layer, Layer) -> IO (WeightMatrix)
 initConnections g (l1,l2) = do
     w <- initMatrix g (neurons l2) (neurons l1)
     return w
@@ -49,16 +55,16 @@ initMlp g ls = do
 
 -- Helper
 
-vecToStr :: Vector Double -> String
+vecToStr :: Vector -> String
 vecToStr v = "[ " ++ intercalate "  " (map show (toList v)) ++ " ]"
 
-vecsToStr :: [Vector Double] -> String
+vecsToStr :: [Vector] -> String
 vecsToStr vs = intercalate "\n" (map vecToStr vs)
 
 -- Layers
 
 data Layer = Layer {
-    activation :: Double -> Double,
+    activation :: FpType -> FpType,
     neurons    :: Int,
     layerType  :: String
 }
@@ -70,14 +76,16 @@ instance Show Layer where
 
 data Network = Network {
     layers  :: [Layer],
-    weights :: [Matrix Double],
+    weights :: [WeightMatrix],
     netType :: String
 }
 
-forward :: Vector Double -> (Layer, Matrix Double) -> Vector Double
-forward x (l,w) = w <> x
+type WeightMatrix = Matrix
 
-activate :: Vector Double -> Network -> [Vector Double]
+forward :: Vector -> (Layer, WeightMatrix) -> Vector
+forward x (l,w) = mapVector (activation l) $ w <> x
+
+activate :: Vector -> Network -> [Vector]
 activate x (Network { layers = ls, weights = ws }) =
      debug result
         where result          = scanl forward input $ zip (drop 1 ls) ws
@@ -85,13 +93,26 @@ activate x (Network { layers = ls, weights = ws }) =
               inputActivation = activation (head ls)
               debug           = trace ("activate input\n" ++ vecToStr x ++ "\n\nactivate output\n" ++ vecsToStr result ++ "\n")
 
+-- Datasets
+
+data Dataset = Dataset [(Vector, Target)]
+data Target = Int | FpType deriving (Eq,Show)
+
+instance Show Dataset where
+    show (Dataset d) = "dataset: " ++ show l ++ " elements, " ++ show u ++ " targets\n"
+        where u = nub (map snd d)
+              l = length d
+
 -- Testing
 
 main :: IO()
 main = do
     g <- newMTGen Nothing
-    let sizes = [2,3,1]
-    let layers = map linearLayer sizes
+    let sizes = [2,5,1]
+    let l1 = linearLayer 2
+    let l2 = sigmoidLayer 3
+    let l3 = linearLayer 1
+    let layers = [l1,l2,l3]
 
     net <- initMlp g layers
 
